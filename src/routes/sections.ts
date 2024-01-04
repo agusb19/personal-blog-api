41259f389e3d374bcac7365448a231346b676897
agusb19 <agustinbrage19@gmail.com>
2024-01-04 12:00:30 -0300

    feat: add multer middleware to parse multipart/form-data

--- src/routes/sections.ts
+++ src/routes/sections.ts
@@ -1,3 +1,4 @@
+import multer from 'multer'
 import { Router } from "express"
 import { Sections as SectionController } from "../controllers/Sections"
 import { type ISection } from "../types/sections"
@@ -11,11 +12,14 @@ type ModelsType = {
 const createSectionRouter = ({ styleModel, sectionModel }: ModelsType) => {
     const sectionRouter = Router()
 
+    const storage = multer.memoryStorage()
+    const upload = multer({ storage: storage })
+
     const sectionController = new SectionController({ styleModel, sectionModel })
 
     sectionRouter.get('/', sectionController.getAll)
-    sectionRouter.put('/', sectionController.changeAll)
-    sectionRouter.post('/', sectionController.addNew)
+    sectionRouter.put('/', upload.single('image'), sectionController.changeAll)
+    sectionRouter.post('/', upload.single('image'), sectionController.addNew)
     sectionRouter.delete('/', sectionController.remove)
 
     return sectionRouter
