e087a1d338741a1a93c03c57d26c1a51d90dd305
agusb19 <agustinbrage19@gmail.com>
2024-01-04 11:57:22 -0300

    feat: add s3 bucket client with its configuration

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,20 @@
-import { createPool } from 'mysql2/promise'
-import { Pool } from 'mysql2/promise'
 import { config } from 'dotenv'
 
 config()
+
+type DBConfig = {
+    host?: string
+    user?: string
+    password?: string
+    database?: string
+}
+
+type BucketConfig = {
+    accessKey?: string
+    bucketName?: string
+    bucketRegion?: string
+    secretAccessKey?: string
+}
 
 export const PORT = process.env.PORT ?? 3000
 export const ENVIRONMENT = process.env.NODE_ENV ?? 'production'
@@ -10,7 +22,7 @@ export const ENVIRONMENT = process.env.NODE_ENV ?? 'production'
 export const JWT_EXPIRE = process.env.JWT_EXPIRE
 export const SECRET_KEY = process.env.SECRET_KEY
 
-export const dbConfig: {[env: string]: object} = {
+export const dbConfig: {[env: string]: DBConfig} = {
     development: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
@@ -29,4 +41,25 @@ export const dbConfig: {[env: string]: object} = {
         password: process.env.TEST_DB_PASSWORD,
         database: process.env.TEST_DB_DATABASE,
     },
+}
+
+export const bucketConfig: {[env: string]: BucketConfig} = {
+    development: {
+        accessKey: process.env.DEV_ACCESS_KEY,
+        bucketName: process.env.DEV_BUCKET_NAME,
+        bucketRegion: process.env.DEV_BUCKET_REGION,
+        secretAccessKey: process.env.DEV_SECRET_ACCESS_KEY,
+    },
+    production: {
+        accessKey: process.env.PROD_ACCESS_KEY,
+        bucketName: process.env.PROD_BUCKET_NAME,
+        bucketRegion: process.env.PROD_BUCKET_REGION,
+        secretAccessKey: process.env.PROD_SECRET_ACCESS_KEY,
+    },
+    test: {
+        accessKey: process.env.TEST_ACCESS_KEY,
+        bucketName: process.env.TEST_BUCKET_NAME,
+        bucketRegion: process.env.TEST_BUCKET_REGION,
+        secretAccessKey: process.env.TEST_SECRET_ACCESS_KEY,
+    }
 }
