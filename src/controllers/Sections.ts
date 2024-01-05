31d90e87e168d11d0d4dc7b36c93522eaae8bc91
agusb19 <agustinbrage19@gmail.com>
2024-01-04 21:23:18 -0300

    feat: add upload image method on add new section

--- src/controllers/Sections.ts
+++ src/controllers/Sections.ts
@@ -1,8 +1,10 @@
+import { s3, bucketName } from '../services/bucket'
+import { PutObjectCommand } from "@aws-sdk/client-s3"
 import { asyncErrorHandler } from "../services/errorHandler"
 import { SectionValidation, type ISectionsValidation } from "../validations/Sections"
 import { createOkResponse, createErrorResponse } from "../helpers/appResponse"
+import type { SectionController, SectionType } from "../types/sections"
 import type { Request, Response } from 'express'
-import { type SectionController } from "../types/sections"
 import { type ISection } from "../types/sections"
 import { type IStyle } from "../types/styles"
 import { type ZodError } from "zod"
@@ -27,6 +29,37 @@ export class Sections implements SectionController {
         return res.status(400).json(createErrorResponse({
             message: 'Validation data error',
             error: validationError.format()
+        }))
+    }
+
+    private async uploadImage(res: Response, data: SectionType['articleIdData'], imageFile?: Express.Multer.File) {
+        
+        if(!imageFile) return res.status(400).json(createErrorResponse({ 
+            message: 'Validation data error, image file required' 
+        }))
+        
+        if(data.image_name === null) return res.status(400).json(createErrorResponse({ 
+            message: 'Validation data error, image name required' 
+        }))
+
+        const command = new PutObjectCommand({
+            Bucket: bucketName,
+            Key: data.image_name,
+            Body: imageFile.buffer,
+            ContentType: imageFile.mimetype
+        })
+
+        await s3.send(command)
+
+        const newIdSection = await this.sectionModel.addNew(data)
+
+        await this.styleModel.addNew({
+            ...data,
+            section_id: newIdSection
+        })
+
+        return res.status(201).json(createOkResponse({
+            message: 'New section image and styles created successfully'
         }))
     }
 
@@ -78,14 +111,14 @@ export class Sections implements SectionController {
 
         if(!validation.success) return this.validationErr(res, validation.error)
 
+        if(validation.data.content_type === 'image') return this.uploadImage(res, validation.data, req.file)
+
         const newIdSection = await this.sectionModel.addNew(validation.data)
 
-        const addNewStyles = {
+        await this.styleModel.addNew({
             ...validation.data,
             section_id: newIdSection
-        }
-
-        await this.styleModel.addNew(addNewStyles)
+        })
 
         return res.status(201).json(createOkResponse({
             message: 'New section content and styles created successfully'
